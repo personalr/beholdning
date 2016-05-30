456b561b118b74c73035b0c9ece237f7c60028d5
Rolf Andal <rolf.andal@gmail.com>
2016-05-30 19:21:38 +0200

    Endret litt på indeks-fil

--- assets/app/app.component.ts
+++ assets/app/app.component.ts
@@ -2,7 +2,7 @@ import {Component} from 'angular2/core';
 @Component({
     selector: 'my-app',
     template: `  
-        <h1>Hello Worlds!</h1>
+        <h1>Hello World!</h1>
     `
 })
 export class AppComponent {
